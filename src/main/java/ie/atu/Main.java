f122bad19ee14e947e2872096ac7dbfe55df73ef
Brendan <G00404587@atu.ie>
2023-11-07 14:03:48 +0000

    Created anArray and put values into array prints each value

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,27 @@
 package ie.atu;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        int[] anArray;
+
+        anArray = new int[10];
+
+        anArray[0] = 100;
+        anArray[1] = 200;
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        int indexValue = 0;
+
+        for (int value: anArray){
+            System.out.println("Element at index  " + indexValue + " : " + value);
+        }
     }
 }
